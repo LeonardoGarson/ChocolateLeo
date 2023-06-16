7fa1df8e10a106a2244fc2590fef8a4c048371fc
LeonardoGarson <Leonardo_ewhg@hotmail.com>
2023-06-15 21:17:12 -0300

    Mudança na palavra Teste

--- src/test/java/apitest/Account.java
+++ src/test/java/apitest/Account.java
@@ -5,6 +5,7 @@ package apitest;
 import com.google.gson.Gson;
 import entities.AccountEntity;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -14,31 +15,37 @@ import static org.hamcrest.Matchers.is;
 public class Account {
     //3.1 Atributos
     String userId;
+    String ct = "application/json"; //ContentType da API
+    String jsonBody; //Guardar o json que será enviado
+    String uri = "https://bookstore.toolsqa.com/Account/v1/"; //Endereço Base
+    Response resposta;
+    String token; //Guardasr o token de identificação do usuário
+
     //3.1.2 - Instanciar Classe Externa
     Gson gson = new Gson(); //Instancia o objeto de conversão de classe para json
 
     //3.2 Métodos e Funções
+
     // Método #1 - Criar usuário
-    @Test
+    @Test(priority = 1)
     public void testCreateUser(){
         //Arrange - Configura
         AccountEntity account = new AccountEntity(); //Chama a entidade usuário
-        account.userName = "LeoGarson1"; //userName - Entrada e saída (resultado esperado)
+        account.userName = "LeoGarson4"; //userName - Entrada e saída (resultado esperado)
         account.password = "12345678@Leo"; //password -   Entrada
 
-        String jsonBody = gson.toJson(account); //Converte a entidade usuário no formato Json
+        jsonBody = gson.toJson(account); //Converte a entidade usuário no formato Json
 
         //Act - Executa
 
         //Dado - Quando - Então
         //Given - When - Then
-        Response resposta = (Response) given()
-                .contentType("application/json")    //Tipo de conteúdo
-                .log().all()                        //Registre tudo
-                .body(jsonBody)                     //Corpo da mensagem
+        resposta = (Response) given()
+                .contentType(ct)        //Tipo de conteúdo
+                .log().all()            //Registre tudo
+                .body(jsonBody)         //Corpo da mensagem
         .when()
-                .post("https://bookstore.toolsqa.com/Account/v1/User")
-
+                    .post(uri + "User")
         //Assert - Valida
         .then()
                 .log().all()                        //Registre tudo na volta
@@ -54,13 +61,58 @@ public class Account {
 
     } //Fim do método de criação do usuário
 
+    @Test(priority = 2)
     public void testGenerateToken(){
         //Configura
-
+            //--> Dados de entrada são fornecidos pela AccountEntity
+            //--> Resultado esperado é que ele receba um token
         //Executa
+           resposta = (Response) given()
+                    .contentType(ct)
+                    .log().all()
+                    .body(jsonBody)
+            .when()
+                    .post(uri + "GenerateToken")
+            .then()
+                   .log().all()
+                   .statusCode(200) // valida a comunicação
+                   .body("status", is("Success")) // Status = Sucesso
+                   .body("result", is("User authorized successfully."))
+                   .extract()
+            ;
+
+       //Extração do toke
+       token = resposta.jsonPath().getString("token");
 
         //Valida
+        Assert.assertTrue(token.length() != 0);
 
     } //Fim de método de criação de usuário
+
+    @Test(priority = 3)
+    public void testAuthorized(){
+        //Configura
+        //Dados de entrada
+        // Serão fornecidos pela AccountEntity através do método testCreateUser - priority = 1
+
+        //Dados de saída / Resultado esperado
+        //StatusCode = 200
+        //Response Body = True
+
+        //Executa
+        given()
+                .contentType(ct)
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri + "Authorized")
+        //Valida
+        .then()
+                .log().all()
+                .statusCode(200)
+                // .body(true) // ToDo: como validar o retorno do body apenas como true
+        ;
+
+    }
 
 }
